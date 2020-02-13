10ebea9d75ef4994d31448200e896dc41a020ea2
Toby Storli <events@stud.ntnu.no>
2020-02-13 16:20:07 +0100

    Ferdig med H_mat, men den er feil, les README

---
+++ README.md
@@ -0,0 +1,4 @@
+*Kvante project 1*
+**Opgave 1**
+Vi får feil verdier for egenveriene til matrisen vår. Jeg har testet med å runde av, for rader og kollonner.
+I følge Mathias skal vi bare få 6 forskjellige, men vi får mange flere.
---
+++ oppgave1.py
@@ -0,0 +1,26 @@
+from scipy.sparse import diags
+import scipy as sp
+import scipy.constants as sc
+import scipy.linalg as la
+import numpy as np
+import matplotlib.pyplot as plt
+
+size = 100
+l = 2*np.pi
+x = np.linspace(0, l, size)
+#h = sc.h
+#hbar = sc.hbar
+#m = sc.m_e
+h = 1
+hbar = 1
+m = 1
+Dx = l/len(x)
+c = 1
+V = np.identity(size)
+
+
+H_dia = np.power(hbar, 2)/(m*np.power(Dx, 2)) + V[0][0]
+H_subDia = -np.power(hbar, 2)/(2*m*np.power(Dx,2)) #−ℏ2/(2𝑚Δ𝑥2)
+
+H = diags([H_subDia, H_dia, H_subDia], [-1, 0, 1], shape=(size, size)).toarray()
+egen_verdi = la.eig(H)
